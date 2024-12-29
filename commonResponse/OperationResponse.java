5c51b2115bcc20602be7df822dfa0b634727214b
Seon <tjswlghks0722@naver.com>
2024-12-29 15:59:26 +0900

    Update OperationResponse.java

--- commonResponse/OperationResponse.java
+++ commonResponse/OperationResponse.java
@@ -1,38 +1,22 @@
-package com.seon.board1.common.response;
-
 import org.springframework.http.HttpStatus;
 
 public class OperationResponse {
+    private OperationResponse(){};
 
-    private String message;
-    private HttpStatus status;
-    private ResponseData<?> data ;
-
-    private OperationResponse(ResponseData<?> data, HttpStatus status, String message) {
-        this.data = data;
-        this.status = status;
-        this.message = message;
+    public static <T> ContentResult<T> contentResult(T data) {
+        return new ContentResult<>(data, HttpStatus.OK, "SUCCESS");
+    }
+    public static <T> ContentResult<T> contentResult(T data, HttpStatus status, String message) {
+        return new ContentResult<>(data, status, message);
     }
 
-    public String getMessage() {
-        return message;
+    public static OperationResult operationResult(boolean operation) {
+        HttpStatus status = operation ? HttpStatus.OK : HttpStatus.BAD_REQUEST;
+        String message = operation ? "SUCCESS" : "FAIL";
+        return new OperationResult(status, message);
     }
 
-    public HttpStatus getStatus() {
-        return status;
-    }
-
-    public ResponseData<?> getData() {
-        return data;
-    }
-
-    public static <T> OperationResponse operationResponse(T data){
-        return new OperationResponse(new ResponseData<>(data), HttpStatus.OK, "SUCCESS");
-    }
-
-    public static OperationResponse operationResponse(boolean success){
-        HttpStatus status = success ? HttpStatus.OK : HttpStatus.INTERNAL_SERVER_ERROR;
-        String message = success ? "SUCCESS" : "FAIL";
-        return new OperationResponse(null, status, message);
+    public static OperationResult operationResult(HttpStatus status, String message) {
+        return new OperationResult(status, message);
     }
 }
